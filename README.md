225e7efd68c319e3a4d37c161dbe684887fcce4e
Lonny Kapelushnik <lonny@lonnylot.com>
2013-12-15 12:04:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# clp-example
+`clp-example` provides some examples of the `github.com/lonnylot/clp` package
+
+## Installation
+Make sure you have a working GO environment. [See the install instructions](http://golang.org/doc/install).
+
+Clone the repo:
+
+    git clone https://github.com/lonnylot/clp-example
+
+Change into the `clp-example` directory, build, and run:
+
+    cd clp-example
+    go build
+    ./clp-example
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"github.com/lonnylot/clp"
+)
+
+func main() {
+	fmt.Println("Bar: Reading Large File")
+	largeFileExample()
+	fmt.Println("Dots: Waiting for a process to complete")
+	dotsExample()
+}
+
+func largeFileExample() {
+	file, _ := os.Open("largeFile")
+	fInfo, _ := file.Stat()
+
+	// Create our clp bar
+	clp := clp.NewBar(fInfo.Size())
+	// Start the output
+	clp.Start()
+
+	b := make([]byte, 1024)
+	for {
+		n, err := file.Read(b)
+
+		// Increment clp
+		clp.Inc(int64(n))
+
+		// Catch our EOL
+		if err != nil {
+			break
+		}
+	}
+
+	// Stop our clp bar
+	clp.Stop()
+}
+
+func dotsExample() {
+	// Create our clp dots
+	clp := clp.NewDots()
+	// Start the output
+	clp.Start()
+
+	t := time.After(time.Duration(time.Second*10))
+	<- t // Wait for the timer...
+
+	// Stop our clp dots
+	clp.Stop()
+}
